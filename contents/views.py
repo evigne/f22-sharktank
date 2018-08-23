ab04893bae41da56aaca7a1db7c64149e963e760
vignesh <vigneshwaranethirajan@gmail.com>
2018-08-23 23:08:31 +0530

    organise files

---
+++ contents/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request,'contents/home.html')
+def abtsharks(request):
+    print ("Hello")
+    # return HttpResponse("<h1>Hello</h1>")
+    return render(request,'contents/abtsharks.html')
+
+
+
